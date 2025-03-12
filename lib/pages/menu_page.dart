4be55d868f1a42d4b2fb1ad6a8c8eb3757677228
Dumpling1902 <rmiadamaris@gmail.com>
2025-03-11 21:37:43 -0600

    Food Details

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:sushi_app_2a_ramirez/components/button.dart';
+import 'package:sushi_app_2a_ramirez/models/food.dart';
 import 'package:sushi_app_2a_ramirez/theme/colors.dart';
+
+import '../components/food_tile.dart';
+import 'food_details_page.dart';
 
 class MenuPage extends StatefulWidget {
   const MenuPage({super.key});
@@ -10,6 +15,24 @@ class MenuPage extends StatefulWidget {
 }
 
 class _MenuPageState extends State<MenuPage> {
+
+  //food menu
+  List foodMenu = [
+    //salmon sushi
+    Food(name: "Salmon Sushi", price: "21.00", imagePath: "lib/images/salmon_sushi.png", rating: "4.9",),
+
+    //tuna
+    Food(name: "Tuna", price: "23.00", imagePath: "lib/images/tuna.png", rating: "4.3",),
+  ];
+
+  //navigate to food item details page
+  void navigateToFoodDetails(int index) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => FoodDetailsPage(
+      food: foodMenu[index],
+    ),),);
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,6 +49,7 @@ class _MenuPageState extends State<MenuPage> {
              style: TextStyle(color: Colors.grey[900]),),
         ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
         //promo banner
         Container(
@@ -34,28 +58,129 @@ class _MenuPageState extends State<MenuPage> {
             borderRadius: BorderRadius.circular(20),
           ),
           margin: const EdgeInsets.symmetric(horizontal: 25),
-          padding: EdgeInsets.all(25),
-          child: Row(children: [
-
+          padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            
+            children: [
             Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 //promo message
-                Text('Get 32% Promo'),
+               Text(
+                'Get 32% Promo', 
+                style: GoogleFonts.dmSerifDisplay(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),),
+
+                const SizedBox(height: 20),
 
                 //redeem button 
                 MyButton(text: 'Redeem', onTap: () {},
                 ),
               ],
-            )
+            ),
+
+            //image
+            Image.asset(
+              'lib/images/many_sushi.png', 
+              height: 100,
+              ),
           
           ],),
-        )
+        ),
+
+        const SizedBox(height: 25),
 
         //search bar
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: TextField(
+            decoration: InputDecoration(
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              enabledBorder:  OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              hintText: "Search here..",
+              ),
+
+            ),
+          ),
+
+          const SizedBox(height: 25),
+        
 
         //menu list
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal:25.0 ),
+          child: Text(
+            "Food Menu", 
+            style: TextStyle(
+              fontWeight: FontWeight.bold, 
+              color: Colors.grey[800],
+              fontSize: 18,
+            ),
+          ),
+        ),
+
+        const SizedBox(height: 10),
+
+        Expanded(
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: foodMenu.length,
+            itemBuilder: (context, index) => FoodTile(
+              food: foodMenu[index],
+              onTap: () => navigateToFoodDetails(index),
+            ),
+          ),
+        ),
+
+        const SizedBox(height: 25),
+
 
         //popular food
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.grey[100],
+            borderRadius: BorderRadius.circular(20),
+          ),
+          margin: EdgeInsets.only(left: 25, right: 25, bottom: 25),
+          padding: EdgeInsets.all(20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+            Row(children: [
+              //image
+            Image.asset('lib/images/salmon_eggs.png', height: 60,),
+
+            const SizedBox(width: 20),
+
+            //name and price
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              //name
+              Text("Salmon Eggs", style: GoogleFonts.dmSerifDisplay(fontSize: 18),),
+
+              const SizedBox(height: 10),
+
+              //price
+              Text('\$21.00', style: TextStyle(color: Colors.grey[700]),),
+            ],
+            ),
+            ],),
+
+            //heart
+            Icon(Icons.favorite_outline, color: Colors.grey, size: 28),
+
+          ],),
+        ),
 
       ],),
     );
